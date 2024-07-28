e206d6c2594e6b610ee57ca739d600a6da9f8dac
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-07-28 16:17:34 +0530

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 import "./footer.css";
-import { FaLinkedinIn } from "react-icons/fa6";
-import { BsInstagram} from "react-icons/bs";
-import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 
-const footer = () => {
+const Footer = () => {
   const currentYear = new Date().getFullYear();
+
   return (
     <footer className="mainContainer" id="Footer">
-      <div className="contentContainer">   
-          <div className="contentText">
-            <h3 className="footerTitle">Social Connection</h3>
-            <ul className="socialLogo">
-              <li>
-                <a href="https://hemanthsbanur.vercel.app/" target="_blank">
-                  <FaGithub />
-				</a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/company/appex-pesu/about/" target="_blank">
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="instalogo">
-                <a href="https://www.instagram.com/appex.pesu/" target="_blank">
-                  <BsInstagram />
-                </a>
-              </li>
-            </ul>
+      <div className="contentContainer">
+        <div className="footerSection contactSection">
+          <h3 className="footerTitle">Contact</h3>
+          <ul className="contactList">
+            <li>Hemanth: 6363546350</li>
+            <li>Atheek: 6361970815</li>
+          </ul>
         </div>
+        
+        <div className="footerSection socialSection">
+          <h3 className="footerTitle">Social Connection</h3>
+          <ul className="socialLogo">
+            <li className="github">
+              <a href="https://github.com/Appex-Pesu/" target="_blank" rel="noopener noreferrer">
+                <FaGithub />
+              </a>
+            </li>
+            <li className="linkedin">
+              <a href="https://www.linkedin.com/company/appex-pesu/about/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="instalogo">
+              <a href="https://www.instagram.com/appex.pesu/" target="_blank" rel="noopener noreferrer">
+                <BsInstagram />
+              </a>
+            </li>
+          </ul>
         </div>
+      </div>
+      
       <div className="copyContainer">
         <p>
           <span>©</span> {currentYear} Appex | Technical <span></span>
@@ -39,4 +48,4 @@ const footer = () => {
   );
 };
 
-export default footer;
+export default Footer;
